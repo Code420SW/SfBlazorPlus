3281dcde8b22d33b955f9e27ec0d020eda868ac8
Code420SW <peter@code420.org>
2022-04-20 06:16:41 -0700

    Back to normal (baseline)

---
+++ SfBlazorPlusServer/SfBlazorPlus/Code/Models/Orchestrator/IOrchestratorParameters.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Code420.SfBlazorPlus.Code.Models.Orchestrator
+{
+    public interface IOrchestratorParameters
+    {
+        List<OrchestratorMenuItem> SidebarMenuItems { get; }
+        List<OrchestratorMenuItem> MainMenuItems { get; }
+        List<OrchestratorMenuItem> FavoritesMenuItems { get; }
+    }
+}
